8e4e47bbb96d085bae8e9a7a3464d9b6794e8783
jeka1331 <mrjeka111@gmail.com>
2023-04-23 22:55:26 +0600

    Add ability to remove tasks from list with confirm Add staged ability to edit tasks Add ability to list tasks

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,94 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
+import { execSync } from "child_process";
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+import { tmpdir } from "os";
 
-import { TaskwarriorTaskProvider, Task } from './tasks';
-
+import { TaskwarriorTaskProvider, Task } from "./tasks";
+import { taskwarriorTasksEditorProvider } from "./taskwarriorTasksEditor";
+import { Uri } from "vscode";
+import {
+  setContext,
+  setDocument,
+  reopenWith,
+  getFileHandler,
+  setTextEditor,
+} from './util';
 export function activate(context: vscode.ExtensionContext) {
+  const taskwarriorTaskProvider = new TaskwarriorTaskProvider();
+  vscode.window.registerTreeDataProvider(
+    "taskwarriorTasks",
+    taskwarriorTaskProvider
+  );
+  vscode.commands.registerCommand("taskwarriorTasks.refreshEntry", () =>
+  taskwarriorTaskProvider.refresh()
+  );
+  // vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
+  vscode.commands.registerCommand("taskwarriorTasks.addEntry", () =>
+    vscode.window.showInformationMessage(`Successfully called add entry.`)
+  );
+  vscode.commands.registerCommand(
+    "taskwarriorTasks.editEntry",
+    async (node: Task) => {
+      // console.log(node);
+      // Create a unique file name with a ".tmp" extension
+      const fileName = `temp-${Math.random()
+        .toString(36)
+        .substring(2, 8)}.task`;
 
-	const nodeDependenciesProvider = new TaskwarriorTaskProvider();
-	vscode.window.registerTreeDataProvider('taskwarriorTasks', nodeDependenciesProvider);
-	vscode.commands.registerCommand('taskwarriorTasks.refreshEntry', () => nodeDependenciesProvider.refresh());
-	// vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	vscode.commands.registerCommand('taskwarriorTasks.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
-	vscode.commands.registerCommand('taskwarriorTasks.editEntry', (node: Task) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	vscode.commands.registerCommand('taskwarriorTasks.deleteEntry', (node: Task) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+      // Get the path to the OS's temporary folder
+      const tempFolder = fs.mkdtempSync(`${tmpdir()}${path.sep}`);
 
-	
+      // Create the file in the temporary folder
+      const filePath = path.join(tempFolder, fileName);
+      fs.writeFileSync(filePath, node.getTaskForEdit());
+
+      // Open the file in the editor
+      // const document = await vscode.workspace.openTextDocument(filePath);
+      // await vscode.window.showTextDocument(document);
+
+      // vscode.workspace.onDidSaveTextDocument((document) => {
+      //   vscode.window.showInformationMessage(`Saved ${document.getText()}`);
+      // });
+
+      await vscode.commands.executeCommand(
+        "vscode.openWith",
+        Uri.file(filePath),
+        "taskwarrior-tasks.editTaskId"
+      );
+
+      // execSync(`EDITOR="code -w" task edit ${node.version}`);
+    }
+  );
+  vscode.commands.registerCommand(
+    "taskwarriorTasks.deleteEntry",
+    async (node: Task) => {
+      vscode.window.showInformationMessage(
+        `Successfully called delete entry on ${node.version}.`,
+        'Yes',
+        'No'
+      ).then(answer => {
+        if (answer === "Yes") {
+          vscode.window.showInformationMessage(node.delete());
+        }
+      });
+    }
+      
+  );
+
+  // const openEditorCommand = vscode.commands.registerCommand(
+  //   "taskwarriorTasks.editEntry",
+  //   () => reopenWith("taskwarrior-tasks.editTask")
+
+  //   // () => reopenWith("taskwarrior-tasks.editTask")
+  // );
+  const openSourceCommand = vscode.commands.registerCommand(
+    "settingsEditor.openSource",
+    () => reopenWith("default")
+  );
+  // context.subscriptions.push(openEditorCommand);
+  context.subscriptions.push(openSourceCommand);
+  context.subscriptions.push(taskwarriorTasksEditorProvider.register(context));
 }
---
+++ src/util.ts
@@ -0,0 +1,86 @@
+import { splitPath } from "licia";
+import * as vscode from "vscode";
+import { endWith } from "licia";
+import path = require("path");
+
+export function setContext(name: string, value: any) {
+  vscode.commands.executeCommand("setContext", name, value);
+}
+
+let document: vscode.TextDocument | undefined;
+export function setDocument(doc: vscode.TextDocument) {
+  document = doc;
+}
+
+export function getDocument() {
+  return document;
+}
+
+let textEditor: vscode.TextEditor | undefined;
+export function setTextEditor(editor: vscode.TextEditor) {
+  textEditor = editor;
+}
+
+export function getTextEditor() {
+  return textEditor;
+}
+
+export function reopenWith(editor: string) {
+  let uri: vscode.Uri | undefined;
+  try {
+    const activeTabInput = vscode.window.tabGroups.activeTabGroup.activeTab
+      ?.input as { [key: string]: any; uri: vscode.Uri | undefined };
+    uri = activeTabInput.uri;
+  } catch (e) {
+    const document = getDocument();
+    if (document) {
+      uri = document.uri;
+    }
+  }
+  if (uri) {
+    vscode.commands.executeCommand("vscode.openWith", uri, editor);
+  }
+}
+
+export async function getFileHandler(document: vscode.TextDocument) {
+  const fileName = document.fileName;
+  const { name, dir } = splitPath(fileName);
+
+  switch (name) {
+    case "app.json":
+    case "project.config.json":
+    case "project.private.config.json":
+    case "project.miniapp.json":
+      return "miniprogram";
+    case "package.json":
+    case ".npmrc":
+      return "npm";
+    case ".prettierrc":
+    case ".prettierrc.json":
+      return "prettier";
+    case "tsconfig.json":
+    case "cypress.json":
+    case ".eslintrc":
+    case ".eslintrc.json":
+    case "lerna.json":
+    case ".nycrc":
+    case ".nycrc.json":
+    case ".babelrc":
+    case ".babelrc.json":
+      return "schema";
+  }
+
+  // miniprogram page
+  if (endWith(name, ".tmp")) {
+    const wxmlPath = path.resolve(dir, name.replace(".json", ".wxml"));
+    const wxmlUri = vscode.Uri.file(wxmlPath);
+    try {
+      await vscode.workspace.fs.stat(wxmlUri);
+      return "miniprogram";
+    } catch (e) {
+      /* empty */
+    }
+  }
+
+  return "";
+}
